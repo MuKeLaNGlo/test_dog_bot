27a67b6ea5b13065e6faea0e3a2a55823a338687
Михаил Каранинский <steammike23@gmail.com>
2023-04-25 21:51:16 +0500

    Удалил две кнопки для управления ботом, отключил звук для уведомления, когда запускается бот

--- services/on_startup_notify.py
+++ services/on_startup_notify.py
@@ -7,6 +7,6 @@ logger = logging.getLogger(__name__)
 
 async def notify_admin(dp: Dispatcher, admin_id: int) -> None:
     try:
-        await dp.bot.send_message(admin_id, 'Бот запущен')
+        await dp.bot.send_message(admin_id, 'Бот запущен', disable_notification=True)
     except Exception as e:
         logger.error(e)
